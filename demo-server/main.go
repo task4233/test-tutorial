e7a10f8d637ad83d89c59eb81551ebb90134a3a2
task4233 <29667656+task4233@users.noreply.github.com>
2021-08-30 22:30:11 +0900

    feat: add demo-server

---
+++ demo-server/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+const (
+	PORT = 8080
+)
+
+type Server struct {
+	handler http.Handler
+}
+
+func (s *Server) init() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", s.Get)
+	r.Post("/", s.Post)
+
+	s.handler = r
+}
+
+func (s *Server) Get(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(5 * time.Second)
+	w.Write([]byte("Get\n"))
+}
+
+func (s *Server) Post(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(5 * time.Second)
+	w.Write([]byte("Post\n"))
+}
+
+func main() {
+	s := &Server{}
+	s.init()
+	http.ListenAndServe(fmt.Sprintf(":%d", PORT), s.handler)
+}
