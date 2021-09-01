bd50dab76f0b3d1444c67be0b937765b6288b799
task4233 <29667656+task4233@users.noreply.github.com>
2021-09-02 01:27:25 +0900

    feat: add code for isucon9

--- locustfile.py
+++ locustfile.py
@@ -1,14 +1,53 @@
-from locust import HttpUser, TaskSet, task
+from locust import HttpUser, TaskSet
 from locust.user.wait_time import between
 
-def index(self):
-    self.client.get("/")
 
-def post(self):
-    self.client.post("/")
+def getNewItem(self):
+    self.client.get("/new_items.json")
+
+
+def getNewCategoryItems(self):
+    self.client.get("/new_items/1.json")
+
+
+def getTransaction(self):
+    self.client.get("/users/transactions.json")
+
+
+def getUserItems(self):
+    self.client.get("/users/1.json")
+
+
+def getItem(self):
+    self.client.get("/items/1.json")
+
+
+def postItemEdit(self):
+    dummy_data = {
+        "csrf_token": "hogefugapiyo",
+        "item_id": 1,
+        "item_price": 101,
+    }
+    self.client.post("/items/edit", headers={"Content-Type": "application/json"}, json=dummy_data)
+
+def postBuy(self):
+    dummy_data = {
+        "csrf_token": "hogefugapiyo",
+        "item_id": 1,
+        "token": "hogefugapiyo",
+    }
+    self.client.post("/buy", headers={"Content-Type": "application/json"}, json=dummy_data)
 
 class UserTasks(TaskSet):
-    tasks = [index, post]
+    tasks = [
+        getNewItem,
+        getNewCategoryItems,
+        getTransaction,
+        getUserItems,
+        getItem,
+        postItemEdit,
+        postBuy,
+    ]
 
     def on_start(self):
         pass
@@ -22,6 +61,6 @@ class UserTasks(TaskSet):
 
 
 class ChiUser(HttpUser):
-    host = "http://127.0.0.1:8080"
+    host = "http://127.0.0.1:8000"
     wait_time = between(2, 5)
     tasks = [UserTasks]
